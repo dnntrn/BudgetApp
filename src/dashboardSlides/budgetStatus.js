dcf0469403622ad268872318bad27eeff252cfae
Deanna <dt1543@nyu.edu>
2019-07-13 16:57:15 -0400

    add progress state to status screen

--- src/dashboardSlides/budgetStatus.js
+++ src/dashboardSlides/budgetStatus.js
@@ -2,25 +2,56 @@ import React, { Component } from 'react'
 
 import { View, ScrollView, Text, StyleSheet, Dimensions } from 'react-native'
 
+import styles from './styles/budgetStatus.style'
+
 const { width } = Dimensions.get('window')
 const height = width * 0.8
 
 class budgetStatus extends Component {
     constructor(props) {
       super(props)
+      this.state = {
+        health: 'on-track',
+      }
     }
 
     render () {
+
+      const { health } = this.state
+
+      //somehow check user's budget totals and change health accordingly
+
+      let color
+      let healthMessage
+
+      switch (health) {
+        case ('on-track'):
+          color = 'green'
+          healthMessage = "Congrats! You're on-track for this month!"
+          break
+
+        case ('tight'):
+          color = 'yellow'
+          healthMessage = "Uh oh. You're budget is looking tight."
+          break
+
+        case ('over'):
+          color = 'red'
+          healthMessage = "Uh oh. You're over budget this month."
+          break
+      }
+
+      // Idea: eventually turn topConatiner into TouchableOpacity so an animation displays on click!
+
       return (
-        <View style={{width: '100%', height: '100%'}}>
-
-          <View style={{backgroundColor: this.props.bgColor, width: '100%', height: '100%'}}>
-            <Text>Your Budget looks great!</Text>
-
-
+        <View style={{width: '100%', padding: '5%'}}>
+          <View style={[styles.container, styles.topContainer, { backgroundColor: color }]}>
+            <Text>{healthMessage}</Text>
           </View>
 
-
+          <View style={[styles.container, styles.spacer, styles.bottomContainer]}>
+            <Text>Here's what you spent on this month:</Text>
+          </View>
         </View>
 
       )
---
+++ src/dashboardSlides/styles/budgetStatus.style.js
@@ -0,0 +1,22 @@
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    borderRadius: 5,
+    padding: '5%',
+  },
+  topContainer: {
+    height: '20%',
+  },
+  spacer: {
+    marginTop: 10,
+  },
+  bottomContainer: {
+    height: '83%',
+    backgroundColor: 'white',
+  },
+})
+
+export default styles
