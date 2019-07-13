3a730d1b9d990d585336cca2ed1241fc8d9aa6bd
Deanna <dt1543@nyu.edu>
2019-07-13 17:34:01 -0400

    add info container to graph screen

--- src/dashboardSlides/budgetGraph.js
+++ src/dashboardSlides/budgetGraph.js
@@ -1,12 +1,36 @@
 import React from 'react'
+
+import { View, Text } from 'react-native'
+
 import { ProgressCircle } from 'react-native-svg-charts'
 
-class ProgressCircleExample extends React.PureComponent {
+import styles from './styles/budgetGraph.style'
+class BudgetGraph extends React.PureComponent {
+
+    constructor(props) {
+      super (props)
+    }
+
     render() {
-        return <ProgressCircle style={{ height: 200 }} progress={0.7} progressColor={'rgb(134, 65, 244)'} />
+
+      // dummy progress value for window
+      // TODO: change progress value based on user's progress in budget
+
+      let progress = 0.7
+
+      let progressPercent = progress * 100
+
+      return (
+        <View style={ styles.container }>
+          <ProgressCircle style={ styles.graph } progress={ progress } progressColor={ 'rgb(134, 65, 244)' } />
+          <View style= { styles.infoContainer }>
+            <Text>{ progressPercent }% of your budget was spent on: </Text>
+          </View>
+        </View>
+      )
     }
 }
 
 
 
-export default ProgressCircleExample
+export default BudgetGraph
---
+++ src/dashboardSlides/styles/budgetGraph.style.js
@@ -0,0 +1,21 @@
+
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  graph: {
+    height: 200,
+  },
+  container: {
+    marginTop: 20,
+    padding: '5%'
+  },
+  infoContainer: {
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: '5%',
+    marginTop: '5%',
+    height: '50%',
+  },
+})
+
+export default styles
